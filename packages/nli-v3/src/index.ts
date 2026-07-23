b920b311d943f02814eaaa8f4278f3681bc1cdec
pymsddf <pymsddf@gmail.com>
2026-07-23 10:17:43 +0530

    feat(nli-v3): add conversational engine with multi-turn memory and clarification

--- packages/nli-v3/src/index.ts
+++ packages/nli-v3/src/index.ts
@@ -1,3 +1,4 @@
 export type { ConversationTurn, ConversationSession, ClarificationQuestion, NLIv3Config, NLIv3Response } from "./types.js";
 export { ConversationManager } from "./conversation.js";
 export { ClarificationEngine } from "./clarification.js";
+export { NLIV3Engine } from "./engine.js";
